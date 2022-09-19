f288cd25b72bdcaa4ba089d9e65377092888d76a
Fabien <fabienswift@gmail.com>
2022-09-19 15:49:44 -0400

    Fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   root to: 'users#index'
 
-  resources :users, only: %i[index show]  do
+  resources :users, only: %i[index show] do
     resources :posts, only: %i[index show]
   end
 end
