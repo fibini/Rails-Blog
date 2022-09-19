7c27a232ea194a09563292ddd4c4f981e11629fa
Fabien <fabienswift@gmail.com>
2022-09-19 15:35:37 -0400

    Add users and posts controllers

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,9 @@
+class PostsController < ApplicationController
+    def index
+        @posts = 'Here is a list of posts for a given user'
+    end
+
+    def show
+        @specific_post = 'This shows a specific post for a specific user'
+    end
+end
