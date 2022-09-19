82bded30bc5742fa226981dc1115df1f7a5f784c
Fabien <fabienswift@gmail.com>
2022-09-19 15:42:28 -0400

    Fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
-    def index
-        @users = 'This page shows all users'
-    end
+  def index
+    @users = 'This page shows all users'
+  end
 
-    def show
-        @specific_user = 'This is a specific user'
-    end
+  def show
+    @specific_user = 'This is a specific user'
+  end
 end
