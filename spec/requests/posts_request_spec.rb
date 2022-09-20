461c983393375a2c09815d629bb7d7b61ce7838e
Fabien <fabienswift@gmail.com>
2022-09-20 19:46:04 -0400

    Add posts request and users request tests

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -2,16 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    it 'returns http success' do
+    it 'returns http success, renders template and includes text' do
       get '/posts/index'
       expect(response).to have_http_status(:success)
+      expect(response).to render_template('posts/index')
+      expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
+    it 'returns http success, renders template and includes text' do
       get '/posts/show'
       expect(response).to have_http_status(:success)
+      expect(response).to render_template('posts/show')
+      expect(response.body).to include('This shows a specific post for a specific user')
     end
   end
 end
