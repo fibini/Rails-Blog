caf4d39f6dc370d1eb2bc7d573f7cf09203608f4
Fabien <fabienswift@gmail.com>
2022-09-22 22:39:18 -0400

    Add Posts funtionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,35 @@ class PostsController < ApplicationController
     @specific_post = Post.find(params[:user_id])
   end
 
+  def new
+    @post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: @post } }
+    end
+  end
+
+  def create
+    # new object from params
+    @post = Post.new(params.require(:post).permit(:title, :text))
+    @post.author = current_user
+    # respond_to block
+    respond_to do |format|
+      format.html do
+        if @post.save
+         # success message
+         flash[:success] = "Post saved successfully"
+         # redirect to index
+         redirect_to user_path(current_user)
+        else
+         # error message
+         flash.now[:error] = "Error: Post could not be saved"
+         # render new
+         render :new, new_user_post_path(current_user)
+        end
+      end
+    end
+  end
+
   def show
     @specific_post = Post.find(params[:id])
     @user = User.find(params[:user_id])
