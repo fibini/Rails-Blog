cb99dfe05199936db9032995e382098eaf9c0138
Fabien <fabienswift@gmail.com>
2022-09-27 15:34:58 -0400

    Add  tests for  views posts/show

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe 'users/show.html.erb', type: :system do
         end
 
         it 'shows post title, post author, comments_count, likes_count' do
-            visit user_posts_path(@first_user, @first_post)
+            visit user_post_path(@first_user, @first_post)
             sleep(3)
             expect(page).to have_content(@first_post.title)
-            expect(page).to have_content(@first_post.author)
-            expect(page).to have_content("Comments: #{first_post.comments_counter}")
-            expect(page).to have_content("Likes: #{first_post.likes_counter}")
+            expect(page).to have_content(@first_post.author.name)
+            expect(page).to have_content("Comments: #{@first_post.comments_counter}")
+            expect(page).to have_content("Likes: #{@first_post.likes_counter}")
         end
     end
 end
