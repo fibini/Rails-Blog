3c4af8e46f47df4e483756a63a04354b4c3813f0
dqarias <dino.quispea@gmail.com>
2022-09-29 16:50:11 -0500

    Update post counter method

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,8 +9,11 @@ class Post < ApplicationRecord
   validates :likes_counter, numericality: { allow_nil: true, only_integer: true, greater_than_or_equal_to: 0 }
 
   after_create :update_posts_count
+  after_destroy :update_posts_count
+
   def update_posts_count
-    author.increment!(:posts_counter)
+    author.posts_counter = author.posts.length
+    author.save
   end
 
   def most_recent_comments
